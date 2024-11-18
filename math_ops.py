aea17244877eee13d407e21b59095025121ff15a
Mika Collins <mikapcollins@gmail.com>
2024-11-17 19:44:55 -0500

    added new-feature branch

--- math_ops.py
+++ math_ops.py
@@ -7,8 +7,17 @@ def divide_numbers(a, b):
         return
     return a / b  
 
+def multiply_numbers(a, b):
+    """Multiplies two numbers and returns the result."""
+    return a * b
+
 if __name__ == "__main__":
     x = 10
     y = 0
     result = divide_numbers(x, y)
     print(f"The result of division is: {result}")
+
+    x = 5
+    y = 3
+    result = multiply_numbers(x, y)
+    print(f"The result of multiplication is: {result}")
